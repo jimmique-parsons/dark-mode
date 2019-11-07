7ce2b5216b5580ab54f017f7351ea7fb591d73e6
Jimmique <jimmique88@gmail.com>
2019-11-07 04:12:46 -0800

    Added functionality to useDarkMode file

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,6 +1,18 @@
-import React, { useState } from "react";
+ import React, { useState, useEffect } from "react";
  import { useLocalStorage } from "./useLocalStorage.js";
 
  export const useDarkMode = (  ) => {
+   const [storedValue, setValue ] = useLocalStorage('darkMode', false);
 
+   const body = document.querySelector('body');
+
+   useEffect(() => {
+     if(storedValue) {
+       body.classList.add('dark-mode');
+     } else {
+       body.classList.remove('dark-mode');
+     }
+   }, [storedValue])
+
+   return [storedValue, setValue];
  }
